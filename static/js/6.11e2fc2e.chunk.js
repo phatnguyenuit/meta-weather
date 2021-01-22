7d19cccd5f6cf52135a574836ff9ec00615577af
Nguyen Phat <phatnt.uit@gmail.com>
2021-01-23 01:03:13 +0700

    Updates

--- static/js/6.11e2fc2e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmeta-weather"]=this["webpackJsonpmeta-weather"]||[]).push([[6],{181:function(t,e,n){"use strict";n.r(e),n.d(e,"NotFoundComponent",(function(){return u}));var o=n(5),a=n(0),u=function(){return Object(o.jsx)("div",{"data-testid":"NotFound",children:"NotFound"})},d=Object(a.memo)(u);d.displayName="NotFound",e.default=d}}]);
-//# sourceMappingURL=6.11e2fc2e.chunk.js.map
