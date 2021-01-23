10f9ae7fa8c12b9b8accaa734745ca20cd4aa417
Nguyen Phat <phatnt.uit@gmail.com>
2021-01-23 10:59:38 +0700

    fix: ignore console.log test basename

--- src/routes/history.ts
+++ src/routes/history.ts
@@ -3,6 +3,7 @@ import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
 
+// eslint-disable-next-line no-console
 console.log('basename', process.env.PUBLIC_URL);
 
 export default history;
