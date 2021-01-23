b18f068f6fe25300efc0aa0ee618c5623f4ce56a
Nguyen Phat <phatnt.uit@gmail.com>
2021-01-23 10:56:02 +0700

    test: url basename

--- src/routes/history.ts
+++ src/routes/history.ts
@@ -2,4 +2,7 @@
 import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory({ basename: process.env.PUBLIC_URL });
+
+console.log('basename', process.env.PUBLIC_URL);
+
 export default history;
