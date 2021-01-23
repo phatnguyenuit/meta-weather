5f7c3bf0bf015d55e11fce1b23720047ff7970b7
Nguyen Phat <phatnt.uit@gmail.com>
2021-01-23 15:46:59 +0700

    test: add e2e test for dashboard and search-city pages

--- cypress/integration/meta-weather/dashboard.spec.ts
+++ cypress/integration/meta-weather/dashboard.spec.ts
@@ -1,9 +1,61 @@
 describe('dashboard', () => {
-  beforeEach(() => {
-    cy.visit('http://localhost:4001');
+  beforeEach(function () {
+    cy.fixture('meta-weather/search-current-location').as('searchLocationData');
+    cy.fixture('meta-weather/weather-at-location').as('weatherAtLocationData');
+    cy.visit(Cypress.env('host'));
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://api.ipify.org',
+      },
+      { fixture: 'meta-weather/ip' },
+    );
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: 'https://ip-geolocation.whoisxmlapi.com',
+      },
+      { fixture: 'meta-weather/location-by-ip' },
+    );
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/location/search',
+      },
+      { fixture: 'meta-weather/search-current-location' },
+    );
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: /\/location\/\d+/,
+      },
+      { fixture: 'meta-weather/weather-at-location' },
+    );
   });
 
-  it('should render dashboard', () => {
-    cy.url().should('include', '/weathers');
+  it('should render dashboard', function () {
+    cy.get('[data-testid="WeatherWidget"]').should(
+      'contain.text',
+      this.searchLocationData[0].title,
+    );
+    const { consolidated_weather } = this.weatherAtLocationData;
+    cy.get('[data-testid="WeatherWidget"]').should(
+      'contain.text',
+      consolidated_weather[0].weather_state_name,
+    );
+    const forecastItems = consolidated_weather.slice(1);
+    cy.get('[data-testid="DailyWeather"]').should(($items) => {
+      // eslint-disable-next-line jest/valid-expect
+      expect($items).to.have.length(forecastItems.length);
+    });
+  });
+
+  it('should be able to search city', function () {
+    cy.get('[data-testid="searchInput"]').type('san{enter}');
+    cy.url().should('contain', '/search?query=san');
   });
 });
---
+++ cypress/integration/meta-weather/weather-search.spec.ts
@@ -0,0 +1,41 @@
+/* eslint-disable jest/valid-expect */
+describe('dashboard', () => {
+  const searchValue = 'san';
+  beforeEach(function () {
+    cy.fixture('meta-weather/search-location').as('searchLocationData');
+    cy.visit(`${Cypress.env('host')}/weathers/search?query=${searchValue}`);
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/location/search',
+      },
+      (req) => {
+        req.reply((res) => {
+          res.delay(1000);
+          res.send({ fixture: 'meta-weather/search-location' });
+        });
+      },
+    ).as('searchLocationRequest');
+  });
+
+  it('should render search page', function () {
+    cy.get("[data-testid='searchInput']").should('have.value', searchValue);
+    cy.get('[data-testid="LoadingPage"]').should('be.visible');
+    cy.contains(`Searching keyword "${searchValue}"....`).should('be.visible');
+
+    cy.wait('@searchLocationRequest');
+    const locationLength = this.searchLocationData.length;
+    cy.contains(`Total found: ${locationLength} result(s)`).should(
+      'be.visible',
+    );
+    cy.get("[data-testid='CityLink']").should(($items) => {
+      expect($items).to.have.length(locationLength);
+    });
+
+    const { woeid } = this.searchLocationData[0];
+    cy.get("[data-testid='CityLink']").eq(0).click();
+
+    cy.url().should('include', `/weathers/${woeid}`);
+  });
+});
