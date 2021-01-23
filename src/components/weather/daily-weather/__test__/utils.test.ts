d652236a99515cd341ed8cf2fac232ca952b717c
Nguyen Phat <phatnt.uit@gmail.com>
2021-01-24 00:32:46 +0700

    fix: add logs for date utils

--- src/components/weather/daily-weather/__test__/utils.test.ts
+++ src/components/weather/daily-weather/__test__/utils.test.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-console */
 import dayjs from 'dayjs';
 import { DATE_FORMAT, TIME_ZONE } from 'constants/common';
 import * as utils from '../utils';
+
+dayjs.tz.setDefault(TIME_ZONE);
 
 describe('formatDate', () => {
   test('should work', () => {
@@ -11,15 +14,18 @@ describe('formatDate', () => {
 
 describe('computeDisplayDate', () => {
   test('should return today', () => {
-    const today = dayjs.tz(dayjs(), TIME_ZONE);
+    const today = dayjs();
+    console.log('today', today.format(DATE_FORMAT));
     expect(utils.computeDisplayDate(today)).toEqual('Today');
   });
   test('should return tomorrow', () => {
-    const tomorrow = dayjs.tz(dayjs(), TIME_ZONE).add(1, 'day');
+    const tomorrow = dayjs().add(1, 'day');
+    console.log('tomorrow', tomorrow.format(DATE_FORMAT));
     expect(utils.computeDisplayDate(tomorrow)).toEqual('Tomorrow');
   });
   test('should return the formatted date', () => {
-    const next2DaysDate = dayjs.tz(dayjs(), TIME_ZONE).add(2, 'day');
+    const next2DaysDate = dayjs().add(2, 'day');
+    console.log('next2DaysDate', next2DaysDate.format(DATE_FORMAT));
     expect(utils.computeDisplayDate(next2DaysDate)).toEqual(
       next2DaysDate.format(DATE_FORMAT),
     );
